9730fefb0b594073bb7d2841a4bac7f155c044b0
13rianVargas <bsvargasc13@gmail.com>
2026-05-29 16:28:07 -0500

    test: add custom elements schema to web specs for shared components

--- Proyecto-Final/frontend/pqrs-app/src/app/web/dashboard/dashboard.page.spec.ts
+++ Proyecto-Final/frontend/pqrs-app/src/app/web/dashboard/dashboard.page.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -38,6 +39,7 @@ describe('DashboardPage', () => {
 
   beforeEach(async () => {
     fixture = TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [DashboardPage],
       imports: [IonicModule, FormsModule],
       providers: [
--- Proyecto-Final/frontend/pqrs-app/src/app/web/tramite/tramite.page.spec.ts
+++ Proyecto-Final/frontend/pqrs-app/src/app/web/tramite/tramite.page.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -40,6 +41,7 @@ describe('TramitePage', () => {
 
   beforeEach(async () => {
     fixture = TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [TramitePage],
       imports: [ReactiveFormsModule, IonicModule],
       providers: [
